bbe33065d238e657a39a8a72f2e09c52fad98078
Christian Jose Atencia Caro <Catencia@intergrupo.com>
2021-08-22 15:51:36 -0500

    Get todo by id

--- Programación_Distribuida.Function/Functions/TodoApi.cs
+++ Programación_Distribuida.Function/Functions/TodoApi.cs
@@ -110,6 +110,66 @@ namespace Programación_Distribuida.Function.Functions
             });
         }
 
+        [FunctionName(nameof(GetAllTodo))]
+        public static async Task<IActionResult> GetAllTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")] HttpRequest req,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation("Get All todos received");
+
+
+            TableQuery<TodoEntity> query = new TableQuery<TodoEntity>();
+            TableQuerySegment<TodoEntity> todos = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+
+
+            string message = "Retrieved all todos";
+            log.LogInformation(message);
+
+
+
+            return new OkObjectResult(new Response
+            {
+                IsSucces = true,
+                Message = message,
+                Result = todos
+            });
+        }
+
+        [FunctionName(nameof(GetTodoById))]
+        public static IActionResult GetTodoById(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")] HttpRequest req,
+           [Table("todo","TODO","{id}", Connection = "AzureWebJobsStorage")] TodoEntity todoEntity,
+           string id,
+           ILogger log)
+        {
+            log.LogInformation($"Get todo by id: {id}, received.");
+
+
+            if (todoEntity == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSucces = false,
+                    Message = "Todo not found."
+                });
+            }
+
+            string message = $"Todo: {todoEntity.RowKey}, retrievet.";
+            log.LogInformation(message);
+
+
+
+            return new OkObjectResult(new Response
+            {
+                IsSucces = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
+
+
+
     }
 
 
